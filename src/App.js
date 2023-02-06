277c3e3129396a324b3be94244376ade38081ae8
Ruben Vizcarra <fabri.vizcarra22@gmail.com>
2023-02-06 16:48:06 -0300

    se creo la rama us002, la carpeta que contendra el formulario del login y el archivo LoginForm

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
+import LoginForm from '../components/Login/LoginForm';
 import './App.css';
+
 
 function App() {
   return (
     <>
       <h1>Beer & Code</h1>
-      
+      <LoginForm/>
     </>
   );
 }
