35f4a86bda5083fc49bcf1bf3c903803e5654438
Candela Mercado <mercadocandela16@gmail.com>
2023-02-16 22:06:35 -0300

    creo una nueva carpeta y modifico la tabla

--- src/components/Admin/Table/ItemsTable.jsx
+++ src/components/Admin/Table/ItemsTable.jsx
@@ -7,20 +7,19 @@ const baseUrl = process.env.REACT_APP_BASE_URL;
 const ItemsTable = () => {
 
     const[items , SetItems] = useState ([]);
-
-    // useEffect (() =>{
-    //     const itemsFetch = async (e) => {
-    //         const data = await axios.get('${baseUrl}/items}');
-    //         SetItems(data.data)
-    //     };
-    //     itemsFetch ();
-    //     []
-    // });
+      useEffect (() =>{
+        const itemsFetch = async (e) => {
+          const data = await axios.get ('${baseUrl}/items}');
+          SetItems(data.data)
+        };
+        itemsFetch ();
+        
+      }, []);
 
     
     return (
 
-        <Table striped bordered hover = 'mt-3'>
+        <Table striped hover = 'mt-3 bg-light rounded'>
         <thead>
           <tr>
             <th>#</th>
@@ -32,24 +31,19 @@ const ItemsTable = () => {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>1</td>
-            <td>Mark</td>
-            <td>Otto</td>
-            <td>@mdo</td>
-          </tr>
-          <tr>
-            <td>2</td>
-            <td>Jacob</td>
-            <td>Thornton</td>
-            <td>@fat</td>
-          </tr>
-          <tr>
-            <td>3</td>
-            <td colSpan={2}>Larry the Bird</td>
-            <td>@twitter</td>
-          </tr>
-        </tbody>
+          {items.map((elemento)=>{
+            return <tr>
+              <td>{elemento.id}</td>
+              <td>{elemento.name}</td>
+              <td>{elemento.price}</td>
+              <td>{elemento.image}</td>
+              <td>{elemento.description}</td>
+             
+            </tr>
+          })}
+          
+         </tbody>
+            
       </Table>
     );
 }
