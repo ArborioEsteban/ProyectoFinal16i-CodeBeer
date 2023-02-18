997ae9bf95b2f4c4e72428972eff6003e93664bc
Ruben Vizcarra <fabri.vizcarra22@gmail.com>
2023-02-17 23:04:01 -0300

    se creó los validadores para los campos del formulario de registro

--- src/components/FormSigIn/FormSignIn.jsx
+++ src/components/FormSigIn/FormSignIn.jsx
@@ -1,37 +1,50 @@
 import React, { useState } from "react";
 import Form from "react-bootstrap/Form";
-import { validateData } from "../../helpers/validateData";
 import { Button, Container, Row, Col } from "react-bootstrap";
-
-
-
-
 
 import { FaFacebookF, FaGoogle } from "react-icons/fa";
 import { HiLogin, HiOutlineClipboardList, HiLockClosed } from "react-icons/hi";
 import { HiEnvelope, HiUser } from "react-icons/hi2";
 
 const FormSignIn = () => {
-  
-  
+
   const [nombreRegistro, setNombreRegistro] = useState();
   const [emailRegistro, setEmailRegistro] = useState();
   const [contraseñaRegistro, setContraseñaRegistro] = useState();
 
+  //const para validar errores en campo email y password
+  const [nombreError, setNombreError] = useState(false);
+  const [emailError, setEmailError] = useState(false);
+  const [passwordError, setPasswordError] = useState(false);
 
   const handleSubmit = (e) =>{
     e.preventDefault();
-    
-    if(validateData(nombreRegistro, emailRegistro, contraseñaRegistro)){
-      console.log("es valido");
+   
+    const nombreRegex = /^[a-zA-Z]{6,20}$/;
+    if(!nombreRegex.test(nombreRegistro.trim())){
+      setNombreError(true);
+      return;
     }
-    else{
-      console.log("no valido");
+    setNombreError(false);
+  // Validar email
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(emailRegistro)) {
+      setEmailError(true);
+      return;
     }
+    setEmailError(false);
+
+ // Validar contraseña
+ const passwordRegex = /^(?=.*[a-zA-Z]{2,})(?=.*\d{2,}).{6,}$/;
+ if (!passwordRegex.test(contraseñaRegistro) || contraseñaRegistro.lenght < 6) {
+   setPasswordError(true);
+   return;
   }
-
-  
-
+  setPasswordError(false);
+ 
+  console.log([nombreRegistro, emailRegistro, contraseñaRegistro])
+   }
+   
   return (
     <div className="container">
       <div className="mt-5 pt-5 px-4 container login rounded ">
@@ -43,7 +56,7 @@ const FormSignIn = () => {
           onSubmit={handleSubmit}
           className="form container py-4 px-4"
         >
-          <Form.Group className="mt-3" controlId="Nombre">
+          <Form.Group className="mt-5" controlId="Nombre">
             <Form.Label>
               <HiUser className="me-2" />
               Ingrese su nombre
@@ -54,9 +67,10 @@ const FormSignIn = () => {
               onChange={(e) =>setNombreRegistro(e.target.value)}
               placeholder="Ingrese su nombre"
             />
+             {nombreError && <span className="helper-text">Ingrese solo letras</span>}
           </Form.Group>
 
-          <Form.Group className="mt-3" controlId="correo">
+          <Form.Group className="mt-5" controlId="correo">
             <Form.Label>
               <HiEnvelope className="me-2" />
               Ingrese su Email
@@ -67,6 +81,7 @@ const FormSignIn = () => {
               onChange={(e)=>setEmailRegistro(e.target.value)}
               placeholder="Ingrese su email"
             />
+             {emailError && <span className="helper-text">El formato del email no es válido.</span>}
           </Form.Group>
           <Form.Group className="my-5" controlId="Password">
                 <Form.Label>
@@ -79,6 +94,7 @@ const FormSignIn = () => {
                   onChange={(e)=>setContraseñaRegistro(e.target.value)}
                   placeholder="****************"
                 />
+                {passwordError && <span className="helper-text">La contraseña debe contener al menos 2 caracteres alfabéticos, 2 caracteres numéricos y tener una longitud mínima de 6 caracteres.</span>}
               </Form.Group>
 
           <Form.Group
