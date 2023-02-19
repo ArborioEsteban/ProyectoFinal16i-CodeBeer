b330be2acbdb1a72b82ceff1143b32f22991d0be
Ruben Vizcarra <fabri.vizcarra22@gmail.com>
2023-02-19 17:00:28 -0300

    se realizo cambios en app.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
-
+import { useEffect, useState } from 'react';
+import { Button, Container } from 'react-bootstrap';
+import ProductGrid from './components/ProductGrid/ProductGrid';
+import SelectTable from './components/Table/SelectTable';
+import Router from "./routing/Router";
+import {BrowserRouter, Routes , Route} from 'react-router-dom';
 import './index.css';
-import Router from './routing/Router';
 
 
 
 const App = () => {
+  useEffect(() => {
+    localStorage.setItem("user","Esteban");
+    // localStorage.setItem("password","123456");
+  }, []);
+  
+
   return (
     <> 
-      <Router />
+      <Container>
+        <Router />  
+      </Container>
+      
     </>
   );
 }
