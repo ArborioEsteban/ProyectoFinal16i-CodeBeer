16d69a3aef44d4e96bc477db7a325e10e2097022
Esteban Arborio <estebanarborio@gmail.com>
2023-02-21 11:07:03 -0300

    corrijo de lugar el mapa funcional

--- src/components/FormularioContacto/FormContacto.jsx
+++ src/components/FormularioContacto/FormContacto.jsx
@@ -170,6 +170,18 @@ const FormContacto = () => {
             </Form>
           </div>
         </div>
+        <section className="w-100 mx-auto text-center pt-2 mb-3">
+        <h1 className="p-4 fs-2 border-top text-light border-3">Direccion</h1>
+
+        <div className="map-responsive w-100 ">
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d14240.408879082126!2d-65.2072018!3d-26.8367009!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94225d3ad7f30f1d%3A0xf8606cd659b8e3e4!2sRollingCode%20School!5e0!3m2!1ses-419!2sar!4v1676604491283!5m2!1ses-419!2sar"
+            loading="lazy"
+            
+          ></iframe>
+        </div>
+      </section>
+        
       </div>
     </div>
   );
